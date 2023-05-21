97b355b642730f022989ab975341bcac3e3120d7
Jarret Nachtigal <jarretn1224@gmail.com>
2023-05-21 02:14:10 -0400

    move player class over from other version

--- player.py
+++ player.py
@@ -2,6 +2,10 @@
 # if the game takes a more PvE route, human and AI players will need to be abstracted somehow
 class Player:
 
-    def __init__(self, player_id, player_name) -> None:
-        self.id = player_id
-        self.player_name = player_name
+    def __init__(self, p_id, name, summons=[], items=[]):
+        self.id = p_id
+        self.name = name
+        # data that will be accessed through a player
+        self.summons = summons  # maybe refer to a Party class?
+        self.items = items  # maybe refer to an Inventory class?
+        self.active_summon = None
